07338614ce3eb4ea95824e7813c47d13bd8c6cf2
Nguyễn Việt Hoàng <146922240+VietHoang-242@users.noreply.github.com>
2024-05-30 22:26:52 +0700

    first commit

---
+++ CTDL_CuoiKyGiaoDienAA/Form1.Designer.cs
@@ -0,0 +1,283 @@
+﻿
+namespace CTDL_CuoiKyGiaoDienAA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gbTu = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtExample = new System.Windows.Forms.TextBox();
+            this.btnDoc = new System.Windows.Forms.Button();
+            this.btnIn = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnLuu = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnThem = new System.Windows.Forms.Button();
+            this.btnXoa = new System.Windows.Forms.Button();
+            this.btnCapNhat = new System.Windows.Forms.Button();
+            this.txtMeaning = new System.Windows.Forms.TextBox();
+            this.txtType = new System.Windows.Forms.TextBox();
+            this.txtWord = new System.Windows.Forms.TextBox();
+            this.lblKQ = new System.Windows.Forms.Label();
+            this.txtTim = new System.Windows.Forms.TextBox();
+            this.btnTim = new System.Windows.Forms.Button();
+            this.gbTu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gbTu
+            // 
+            this.gbTu.Controls.Add(this.label4);
+            this.gbTu.Controls.Add(this.txtExample);
+            this.gbTu.Controls.Add(this.btnDoc);
+            this.gbTu.Controls.Add(this.btnIn);
+            this.gbTu.Controls.Add(this.btnExit);
+            this.gbTu.Controls.Add(this.btnLuu);
+            this.gbTu.Controls.Add(this.label3);
+            this.gbTu.Controls.Add(this.label2);
+            this.gbTu.Controls.Add(this.label1);
+            this.gbTu.Controls.Add(this.btnThem);
+            this.gbTu.Controls.Add(this.btnXoa);
+            this.gbTu.Controls.Add(this.btnCapNhat);
+            this.gbTu.Controls.Add(this.txtMeaning);
+            this.gbTu.Controls.Add(this.txtType);
+            this.gbTu.Controls.Add(this.txtWord);
+            this.gbTu.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gbTu.Location = new System.Drawing.Point(29, 36);
+            this.gbTu.Name = "gbTu";
+            this.gbTu.Size = new System.Drawing.Size(410, 467);
+            this.gbTu.TabIndex = 0;
+            this.gbTu.TabStop = false;
+            this.gbTu.Text = "NHẬP THÔNG TIN";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(14, 207);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 19);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "VÍ DỤ";
+            // 
+            // txtExample
+            // 
+            this.txtExample.Location = new System.Drawing.Point(101, 199);
+            this.txtExample.Name = "txtExample";
+            this.txtExample.Size = new System.Drawing.Size(290, 27);
+            this.txtExample.TabIndex = 12;
+            // 
+            // btnDoc
+            // 
+            this.btnDoc.Location = new System.Drawing.Point(22, 397);
+            this.btnDoc.Name = "btnDoc";
+            this.btnDoc.Size = new System.Drawing.Size(79, 45);
+            this.btnDoc.TabIndex = 11;
+            this.btnDoc.Text = "Đọc";
+            this.btnDoc.UseVisualStyleBackColor = true;
+            this.btnDoc.Click += new System.EventHandler(this.btnDoc_Click);
+            // 
+            // btnIn
+            // 
+            this.btnIn.Location = new System.Drawing.Point(139, 322);
+            this.btnIn.Name = "btnIn";
+            this.btnIn.Size = new System.Drawing.Size(90, 41);
+            this.btnIn.TabIndex = 10;
+            this.btnIn.Text = "In";
+            this.btnIn.UseVisualStyleBackColor = true;
+            this.btnIn.Click += new System.EventHandler(this.btnIn_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(261, 322);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(120, 41);
+            this.btnExit.TabIndex = 1;
+            this.btnExit.Text = "Thoát";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnLuu
+            // 
+            this.btnLuu.Location = new System.Drawing.Point(22, 321);
+            this.btnLuu.Name = "btnLuu";
+            this.btnLuu.Size = new System.Drawing.Size(79, 42);
+            this.btnLuu.TabIndex = 2;
+            this.btnLuu.Text = "Lưu";
+            this.btnLuu.UseVisualStyleBackColor = true;
+            this.btnLuu.Click += new System.EventHandler(this.btnLuu_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(18, 61);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 19);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "TỪ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(2, 110);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 19);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "LOẠI TỪ";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 160);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 19);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "NGHĨA";
+            // 
+            // btnThem
+            // 
+            this.btnThem.Location = new System.Drawing.Point(22, 254);
+            this.btnThem.Name = "btnThem";
+            this.btnThem.Size = new System.Drawing.Size(79, 41);
+            this.btnThem.TabIndex = 6;
+            this.btnThem.Text = "Thêm";
+            this.btnThem.UseVisualStyleBackColor = true;
+            this.btnThem.Click += new System.EventHandler(this.btnThem_Click);
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.Location = new System.Drawing.Point(139, 254);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(90, 41);
+            this.btnXoa.TabIndex = 5;
+            this.btnXoa.Text = "Xoá";
+            this.btnXoa.UseVisualStyleBackColor = true;
+            this.btnXoa.Click += new System.EventHandler(this.btnXoa_Click);
+            // 
+            // btnCapNhat
+            // 
+            this.btnCapNhat.Location = new System.Drawing.Point(261, 254);
+            this.btnCapNhat.Name = "btnCapNhat";
+            this.btnCapNhat.Size = new System.Drawing.Size(120, 41);
+            this.btnCapNhat.TabIndex = 4;
+            this.btnCapNhat.Text = "Cập Nhật";
+            this.btnCapNhat.UseVisualStyleBackColor = true;
+            this.btnCapNhat.Click += new System.EventHandler(this.btnCapNhat_Click);
+            // 
+            // txtMeaning
+            // 
+            this.txtMeaning.Location = new System.Drawing.Point(101, 152);
+            this.txtMeaning.Name = "txtMeaning";
+            this.txtMeaning.Size = new System.Drawing.Size(290, 27);
+            this.txtMeaning.TabIndex = 1;
+            // 
+            // txtType
+            // 
+            this.txtType.Location = new System.Drawing.Point(101, 102);
+            this.txtType.Name = "txtType";
+            this.txtType.Size = new System.Drawing.Size(290, 27);
+            this.txtType.TabIndex = 2;
+            // 
+            // txtWord
+            // 
+            this.txtWord.Location = new System.Drawing.Point(101, 53);
+            this.txtWord.Name = "txtWord";
+            this.txtWord.Size = new System.Drawing.Size(290, 27);
+            this.txtWord.TabIndex = 3;
+            // 
+            // lblKQ
+            // 
+            this.lblKQ.AutoSize = true;
+            this.lblKQ.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblKQ.Location = new System.Drawing.Point(471, 110);
+            this.lblKQ.Name = "lblKQ";
+            this.lblKQ.Size = new System.Drawing.Size(58, 19);
+            this.lblKQ.TabIndex = 11;
+            this.lblKQ.Text = "TuDien";
+            // 
+            // txtTim
+            // 
+            this.txtTim.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTim.Location = new System.Drawing.Point(579, 61);
+            this.txtTim.Name = "txtTim";
+            this.txtTim.Size = new System.Drawing.Size(348, 27);
+            this.txtTim.TabIndex = 1;
+            // 
+            // btnTim
+            // 
+            this.btnTim.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTim.Location = new System.Drawing.Point(462, 50);
+            this.btnTim.Name = "btnTim";
+            this.btnTim.Size = new System.Drawing.Size(93, 38);
+            this.btnTim.TabIndex = 0;
+            this.btnTim.Text = "Tìm";
+            this.btnTim.UseVisualStyleBackColor = true;
+            this.btnTim.Click += new System.EventHandler(this.btnTim_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(974, 553);
+            this.Controls.Add(this.lblKQ);
+            this.Controls.Add(this.txtTim);
+            this.Controls.Add(this.gbTu);
+            this.Controls.Add(this.btnTim);
+            this.Name = "Form1";
+            this.Text = "TuDienAnhAnh";
+            this.gbTu.ResumeLayout(false);
+            this.gbTu.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gbTu;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnLuu;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnThem;
+        private System.Windows.Forms.Button btnXoa;
+        private System.Windows.Forms.Button btnCapNhat;
+        private System.Windows.Forms.TextBox txtMeaning;
+        private System.Windows.Forms.TextBox txtType;
+        private System.Windows.Forms.TextBox txtWord;
+        private System.Windows.Forms.Label lblKQ;
+        private System.Windows.Forms.TextBox txtTim;
+        private System.Windows.Forms.Button btnTim;
+        private System.Windows.Forms.Button btnIn;
+        private System.Windows.Forms.Button btnDoc;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtExample;
+    }
+}
+
---
+++ CTDL_CuoiKyGiaoDienAA/Form1.cs
@@ -0,0 +1,274 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+
+namespace CTDL_CuoiKyGiaoDienAA
+{
+    public partial class Form1 : Form
+    {
+        public struct Word
+        {
+            public string WordName;
+            public string Type;
+            public string Meaning;
+            public string Example;
+        }
+        public class DictNode
+        {
+            public Word Word;
+            public DictNode Next;
+        }
+        public class HashTable
+        {
+            public int M;
+            public DictNode[] heads;
+
+            public HashTable()      
+            {
+                M = 100;
+                heads = new DictNode[M];
+                Init(heads);
+            }
+
+            public void Init(DictNode[] heads)
+            {
+                for (int i = 0; i < heads.Length; i++)
+                {
+                    heads[i] = null;
+                }
+            }
+
+            public int hashFunc(Word w)
+            {
+                return hash1(w.WordName);
+            }
+
+            public int hash1(string en)
+            {
+                // Tính mã băm dựa trên mã ASCII chia cho tổng số phần tử của mảng băm
+                int h = 0;
+                for (int i = 0; i < en.Length; i++)
+                {
+                    h += ((int)char.ToLower(en[i])) * (i + 1);
+                }
+
+                return h % M;
+            }
+            public DictNode createWord(Word w)
+            {
+                DictNode word = new DictNode();
+                word.Word = w;
+                word.Next = null;
+                return word;
+            }
+            public void addWord(DictNode[] heads, Word w)
+            {
+                int h = hashFunc(w);
+                DictNode r = heads[h];
+                DictNode p = createWord(w);
+                if (r == null)
+                {
+                    heads[h] = p;
+                }
+                else
+                {
+                    while (r.Next != null)
+                    {
+                        r = r.Next;
+                    }
+
+                    r.Next = p;
+                }
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        HashTable hash = new HashTable();   //Khởi tạo hashtable hash
+        private void btnThem_Click(object sender, EventArgs e)
+        {
+            //Khởi tạo Word w
+            Word w = new Word();
+            w.WordName = txtWord.Text.Trim();       //Gán nội dung của textbox txtWord cho WordName
+            w.Type = txtType.Text.Trim();           //Gán nội dung của textbox txtType cho Type
+            w.Meaning = txtMeaning.Text.Trim();      //Gán nội dung của textbox txtMeaning cho Meaning
+            w.Example = txtExample.Text.Trim();       //Gán nội dung của textbox txtExample cho Example
+
+
+            hash.addWord(hash.heads, w);        // Sử dụng phương thức addWWord để thêm từ vào hash.
+
+            // Xoá trắng các textbox sau khi đã nhập xong từ
+            txtWord.ResetText();
+            txtType.ResetText();
+            txtMeaning.ResetText();
+            txtExample.ResetText();
+            // Đưa trỏ chuột về lại textbox txtWord
+            txtWord.Focus();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();     //Thoát chương trình 
+        }
+
+        private void btnIn_Click(object sender, EventArgs e)
+        {
+            lblKQ.Text = " ";   //Xoá trắng label lblKQ sau mỗi lần nhấn
+            // Duyệt qua tất cả các phần tử của hashtable
+            for (int i = 0; i < hash.M; i++)
+            {
+                // phần tử nào có từ thì in lần lượt từ, loại từ, nghĩa, ví dụ của từ
+                if (hash.heads[i] != null)
+                {
+                    DictNode p = hash.heads[i];
+                    while (p != null)
+                    {
+                        lblKQ.Text = lblKQ.Text + "=============================" + "\n";
+                        lblKQ.Text = lblKQ.Text + p.Word.WordName + "\n";
+                        lblKQ.Text = lblKQ.Text + p.Word.Type + "\n";
+                        lblKQ.Text = lblKQ.Text + p.Word.Meaning + "\n";
+                        lblKQ.Text = lblKQ.Text + p.Word.Example + "\n";
+                        p = p.Next;
+                    }
+                }
+            }
+
+        }
+
+        private void btnCapNhat_Click(object sender, EventArgs e)
+        {
+            Word w = new Word();
+            w.WordName = txtWord.Text.Trim();       //Gán nội dung của textbox txtWord cho WordName
+            w.Type = txtType.Text.Trim();           //Gán nội dung của textbox txtType cho Type
+            w.Meaning = txtMeaning.Text.Trim();      //Gán nội dung của textbox txtMeaning cho Meaning
+            w.Example = txtExample.Text.Trim();       //Gán nội dung của textbox txtExample cho Example
+
+
+
+            int h = hash.hashFunc(w);            //Lấy mã hashFunc của từ cần cập nhập
+            DictNode r = hash.heads[h];
+            if (r != null)
+            {
+                int t = hash.hashFunc(r.Word);
+                do
+                {
+                    t = hash.hashFunc(r.Word);
+                    if (t == h)
+                    {
+                        r.Word = w;
+                        break;
+                    }
+
+                    r = r.Next;
+                } while (r != null);
+            }
+        }
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            string en = txtWord.Text.Trim();        //Gán từ cần xoá được nhập vào từ textbox txtWord cho biến en. 
+            int h = hash.hash1(en);                 // Lấy mã hash của từ en lưu vào biến h
+            DictNode r = hash.heads[h];
+            DictNode prev = null;
+            while (r != null)
+            {
+                if (r.Word.WordName.CompareTo(en) == 0)
+                {
+                    if (prev == null)        //Xóa nếu en là đầu danh sách
+                    {
+                        hash.heads[h] = r.Next;
+                    }
+                    else                      //Xóa ở vị trí bất kì của en 
+                    {
+                        prev.Next = r.Next;
+                    }
+                    r.Next = null;
+                    r = r.Next;
+                    break;
+                }
+                prev = r;
+                r = r.Next;
+            }
+        }
+
+        private void btnTim_Click(object sender, EventArgs e)
+        {
+            string en = txtTim.Text.Trim();      //Gán từ cần xoá được nhập vào từ textbox txtWord cho biến en. 
+
+            // Lấy mã hash của từ en lưu vào biến h
+            int h = hash.hash1(en);
+            DictNode r = hash.heads[h];
+            while (r != null)
+            {
+                // Tìm kiếm theo thuật toán tìm kiếm tuần tự (Linear Search)
+                int h1 = hash.hash1(r.Word.WordName);
+                if (h == h1)  
+                {
+                    lblKQ.Text = r.Word.WordName + "\n" + r.Word.Type + "\n" + r.Word.Meaning + "\n" + r.Word.Example;
+                    break;
+                }
+                r = r.Next;
+            }
+        }
+
+        private void btnDoc_Click(object sender, EventArgs e)
+        {
+            int i = 0, k = 1, z = 2, j = 3;
+            // đọc tất cả các hàng của file 'dicts.txt' vào chuỗi wordData
+            string[] wordData = File.ReadAllLines("dicts.txt");
+
+            while (z <= wordData.Length)
+            {
+                Word data = new Word
+                {
+                    WordName = wordData[i],
+                    Type = wordData[k],
+                    Meaning = wordData[z],
+                    Example = wordData[j]
+                };
+                i = i + 4;
+                k = k + 4;
+                z = z + 4;
+                j = j + 4;
+
+                hash.addWord(hash.heads, data);
+            }
+            // Thông báo khi đã đọc xong file
+            lblKQ.Text = "File da duoc doc xong";
+        }
+
+        private void btnLuu_Click(object sender, EventArgs e)
+        {
+            FileStream fs = new FileStream("tudienAnhAnh.txt", FileMode.Create, FileAccess.ReadWrite);
+            StreamWriter swrite = new StreamWriter(fs);
+            // Duyệt lần lượt các phần tử của hashtable nếu phần tử nào có từ thì lưu từ, loại từ, nghĩa, ví dụ của từ vào file.
+            for (int i = 0; i < hash.M; i++)
+            {
+                if (hash.heads[i] != null)
+                {
+                    DictNode p = hash.heads[i];
+                    while (p != null)
+                    {
+                        swrite.WriteLine(p.Word.WordName);
+                        swrite.WriteLine(p.Word.Type);
+                        swrite.WriteLine(p.Word.Meaning);
+                        swrite.WriteLine(p.Word.Example);
+                        p = p.Next;
+                    }
+                }
+            }
+            swrite.Flush();
+            fs.Close();     // Đóng file sau khi đã ghi xong.
+            //Thông Báo sau khi đã lưu xong vào file
+            lblKQ.Text = "TuDienDaDuocLuuVaoFile";
+        }
+    }
+}
